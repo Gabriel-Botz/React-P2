bc7c9c50d4aaa1a081ec105779fe3d51dd9db907
Caio Vinicius Dias <dev.caiodias19@gmail.com>
2026-06-10 15:31:47 -0300

    feat: SuspectDetails consumindo contexto

--- src/pages/SuspectDetails/SuspectDetails.jsx
+++ src/pages/SuspectDetails/SuspectDetails.jsx
@@ -1,8 +1,13 @@
 import {useParams} from "react-router-dom";
+import {useContext} from "react";
+import {InvestigationContext} from "../../contexts/InvestigationContext";
 
 function SuspectDetails () {
     const { id } = useParams();
+    const { suspects } = context(InvestigationContext);
     
+    const suspect = suspects.find( s => s.id === Number(id) );
+
     console.log("Suspeitos como o id: ", id);
 
     return (
