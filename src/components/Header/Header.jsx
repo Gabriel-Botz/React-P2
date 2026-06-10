51062ab35ec1c7e8a5acf35e78f9e5631abd2970
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-06-10 16:35:42 -0300

    finalizando estilo header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,25 @@
 import style from "./Header.module.css";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/icons/lupa.png"
 
 function Header () {
+
+    const getLinkClass = ({ isActive }) => isActive ? style.active : style.item;
+    
     return (
     <header className={style.header}>
         <div className={style.container}>
         <div className={style.logoContainer}>
         <img className={style.logo} src={logo} alt="logo"/>
-        <h1 className={style.title}>The Last Clue</h1>
+        <h1><Link className={style.title} to="/">The Last Clue</Link></h1>
         </div>
         <div className={style.menu}>
             <ul className={style.list}>
-                <li><Link className={style.item} to="/">Caso</Link></li>
-                <li><Link className={style.item} to="/suspect">Suspeitos</Link></li>
-                <li><Link className={style.item} to="/clues">Pistas</Link></li>
-                <li><Link className={style.item} to="/witness">Testemunhas</Link></li>
-                <li><Link className={style.item} to="/accusation">Acusar</Link></li>
+                <li><NavLink className={getLinkClass} to="/">Caso</NavLink></li>
+                <li><NavLink className={getLinkClass} to="/suspect">Suspeitos</NavLink></li>
+                <li><NavLink className={getLinkClass} to="/clues">Pistas</NavLink></li>
+                <li><NavLink className={getLinkClass} to="/witness">Testemunhas</NavLink></li>
+                <li><NavLink className={getLinkClass} to="/accusation">Acusar</NavLink></li>
             </ul>
         </div>
         </div>
