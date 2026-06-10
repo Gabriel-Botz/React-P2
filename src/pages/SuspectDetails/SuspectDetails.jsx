bcd13d69fd140df00e4815e23d5fd502181b4935
Caio Vinicius Dias <dev.caiodias19@gmail.com>
2026-06-10 14:12:00 -0300

    feat: criando o esqueleto de SuspectDetails.jsx

--- src/pages/SuspectDetails/SuspectDetails.jsx
+++ src/pages/SuspectDetails/SuspectDetails.jsx
@@ -1,4 +1,12 @@
+import {useParams} from "react-router-dom";
+
 function SuspectDetails () {
-    return <p>Suspect Details</p>
+    const { id } = useParams();
+    
+    console.log("Suspeitos como o id: ", id);
+
+    return (
+        <p>Detalhes do suspeito com o id: {id}</p>
+    )
 };
 export default SuspectDetails;
