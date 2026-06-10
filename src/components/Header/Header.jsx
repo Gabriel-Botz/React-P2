5c90cf8e985b4cf97fbf9b0ceb74615387b0e7c2
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-06-10 15:40:39 -0300

    ajuste de estilo header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,19 +4,21 @@ import logo from "../../assets/icons/lupa.png"
 
 function Header () {
     return (
-    <header className={style.header}> 
-        <div className={style.logoContainer}> 
+    <header className={style.header}>
+        <div className={style.container}>
+        <div className={style.logoContainer}>
         <img className={style.logo} src={logo} alt="logo"/>
         <h1 className={style.title}>The Last Clue</h1>
         </div>
         <div className={style.menu}>
             <ul className={style.list}>
-                <li className={style.item}><Link to="/">Caso</Link></li>
-                <li className={style.item}><Link to="/suspect">Suspeitos</Link></li>
-                <li className={style.item}><Link to="/clues">Pistas</Link></li>
-                <li className={style.item}><Link to="/witness">Testemunhas</Link></li>
-                <li className={style.item}><Link to="/accusation">Acusar</Link></li>
+                <li><Link className={style.item} to="/">Caso</Link></li>
+                <li><Link className={style.item} to="/suspect">Suspeitos</Link></li>
+                <li><Link className={style.item} to="/clues">Pistas</Link></li>
+                <li><Link className={style.item} to="/witness">Testemunhas</Link></li>
+                <li><Link className={style.item} to="/accusation">Acusar</Link></li>
             </ul>
+        </div>
         </div>
     </header>
 )};
