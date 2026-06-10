b583919910865b07363cdfd753a524d66b3c30e5
Caio Vinicius Dias <dev.caiodias19@gmail.com>
2026-06-10 16:53:15 -0300

    feat: imprimindo os detalhes do suspeito

--- src/pages/SuspectDetails/SuspectDetails.jsx
+++ src/pages/SuspectDetails/SuspectDetails.jsx
@@ -1,17 +1,24 @@
 import {useParams} from "react-router-dom";
 import {useContext} from "react";
-import {InvestigationContext} from "../../contexts/InvestigationContext";
+import {useInvestigation} from "../../context/InvestigationContext";
 
 function SuspectDetails () {
     const { id } = useParams();
-    const { suspects } = context(InvestigationContext);
+    const { suspects } = useInvestigation();
+    const [loading, setLoading] = useState(false);
     
     const suspect = suspects.find( s => s.id === Number(id) );
 
-    console.log("Suspeitos como o id: ", id);
-
+    if (loading) return <Loading/> // ainda precisa ser criada essa função
+    if (!suspect) return <p>Suspeito não encontrado</p>;
+    
     return (
-        <p>Detalhes do suspeito com o id: {id}</p>
-    )
+        <div>
+            <h1>{suspect.name}</h1>
+            <p>{suspect.profissao}</p>
+            <p>{suspect.motivo}</p>
+            <p>{suspect.alibi}</p>
+        </div>
+    );
 };
 export default SuspectDetails;
