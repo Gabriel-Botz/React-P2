e40c2e9f1162509091c9b575318c1ead01ed5efd
marceloprmayrinck <marceloprmayrinck@gmail.com>
2026-06-11 16:05:33 -0300

    Criaçao da pagina home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -21,16 +21,23 @@ function Home({}) {
               Na noite de sexta-feira, Roberto Valente foi encontrado sem vida
               sobre sua escrivaninha. Um copo de uísque quebrado no chão e uma
               mancha escura de bebida indicam que o veneno foi consumido pouco
-              antes de sua morte. O sistema de segurança da mansão registrou a
-              entrada de poucas pessoas naquele dia. O delegado responsável
-              filtrou 4 suspeitos principais que tinham acesso à mansão ou
-              motivos comerciais e pessoais explícitos para desejar o fim de
-              Roberto. Além disso, 4 testemunhas chaves próximas prestaram
-              depoimentos preliminares. Como detetive encarregado, sua missão é
-              cruzar os fatos, vasculhar os “8 indícios” espalhados ao redor dos
-              documentos. Sua descoberta revelará incongruências óbvias no álibi
-              de quem cometeu o crime. Uma acusação sem provas sólidas permitirá
-              que o verdadeiro assassino escape.
+              antes de sua morte.{" "}
+              <p>
+                O sistema de segurança da mansão registrou a entrada de poucas
+                pessoas naquele dia. O delegado responsável filtrou 4 suspeitos
+                principais que tinham acesso à mansão ou motivos comerciais e
+                pessoais explícitos para desejar o fim de Roberto. Além disso, 4
+                testemunhas chaves próximas prestaram depoimentos
+                preliminares.{" "}
+              </p>
+              <p>
+                {" "}
+                Como detetive encarregado, sua missão é cruzar os fatos,
+                vasculhar os “8 indícios” espalhados ao redor dos documentos.
+                Sua descoberta revelará incongruências óbvias no álibi de quem
+                cometeu o crime. Uma acusação sem provas sólidas permitirá que o
+                verdadeiro assassino escape.
+              </p>
             </p>
 
             <button className={styles.button}>Iniciar investigação</button>
