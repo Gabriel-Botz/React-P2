d8fe30132794df996dc36d697002d33198501afb
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-06-11 16:12:37 -0300

    adicionando backend

---
+++ backend/data/mockCase.js
@@ -0,0 +1,19 @@
+const mockCase = {
+  caso: {
+    id: "caso-001",
+    titulo: "O Último Indício",
+  },
+
+  suspeitos: [
+    {
+      id: "suspeito-1",
+      nome: "Lucas Ferreira",
+    },
+    {
+      id: "suspeito-2",
+      nome: "Amanda Ribeiro",
+    },
+  ],
+};
+
+export default mockCase;
