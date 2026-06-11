c28f1280bf0b31e8c0a9a610514ddb72762f3010
Caio Vinicius Dias <dev.caiodias19@gmail.com>
2026-06-11 20:52:00 -0300

    feat: iniciando a estilização do Modal de acusação

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,11 +3,28 @@ function Modal({isOpen, suspectName, onConfirm, onCancel}) {
     if (!isOpen) return null;
 
     return (
-        <div>
-            <div>
-                <p> Deseja acusar <strong>{suspectName}</strong>?</p>
-                <button onClick={onConfirm}>Confirmar</button>
-                <button onClick={onCancel}>Cancelar</button>
+        <div className="overlay">
+            <div className="modal">
+
+                <div className="modal-header">
+                    <span>⚠️</span>
+                    <h2>ATENÇÃO DETETIVE</h2>
+                </div>
+
+                <p>Tem certeza que deseja acusar este suspeito?</p>
+
+                <div className="suspect-box">
+                    <small>SUSPEITO SELECIONADO:</small>
+                    <strong>{suspectName}</strong>
+                </div>
+
+                <p><em>Esta decisão é definitiva e encerrará o caso.</em></p>
+
+                <div className="modal-buttons">
+                    <button onClick={onConfirm}>Confirmar</button>
+                    <button onClick={onCancel}>Cancelar</button>
+                </div>
+
             </div>
         </div>
     )
