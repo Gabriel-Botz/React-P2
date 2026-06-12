0274a79ed7bfb28a7889ca805f091e0b1f25f18f
marceloprmayrinck <marceloprmayrinck@gmail.com>
2026-06-12 14:13:33 -0300

    feat: implementação do card de crime

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,20 +16,37 @@ function Home({}) {
             <h1 className={styles.casoTitulo}>O Ultimo Indicio</h1>
             <div className={styles.barraTitulo} />
 
-            <p className={styles.resumo}>Resumo do crime</p>
-            <p className={styles.descricao}>
+            <h3 className={styles.sumario}>Sumário do Crime</h3>
+            <div className={styles.crimeCard}>
+              <div className={styles.crime}>
+                <p className={styles.crimeValue} id="vitima">
+                  <span className={styles.crimeDesc}>Vitima: </span>
+                  Dr Hans Chucrute, 70 anos, medico de passarinho
+                </p>
+
+                <p className={styles.crimeValue} id="local">
+                  <span className={styles.crimeDesc}>Local: </span>
+                  Escritorio principal do Hospital Estadual Pica-pau
+                </p>
+
+                <p className={styles.crimeValue} id="hora">
+                  <span className={styles.crimeDesc}>Hora do Obito: </span>
+                  Sexta-feira, estimada entre 22h e 23h
+                </p>
+
+                <p className={styles.crimeValue} id="causa">
+                  <span className={styles.crimeDesc}>Causa da Morte: </span>
+                  Asfixia por envenenamento e comida de passarinho
+                </p>
+              </div>
+            </div>
+
+            <h4 className={styles.sumario}>Resumo do crime</h4>
+            <p className={styles.descricao} id="descricao">
               Na noite de sexta-feira, Roberto Valente foi encontrado sem vida
               sobre sua escrivaninha. Um copo de uísque quebrado no chão e uma
               mancha escura de bebida indicam que o veneno foi consumido pouco
               antes de sua morte.{" "}
-              <p>
-                O sistema de segurança da mansão registrou a entrada de poucas
-                pessoas naquele dia. O delegado responsável filtrou 4 suspeitos
-                principais que tinham acesso à mansão ou motivos comerciais e
-                pessoais explícitos para desejar o fim de Roberto. Além disso, 4
-                testemunhas chaves próximas prestaram depoimentos
-                preliminares.{" "}
-              </p>
               <p>
                 {" "}
                 Como detetive encarregado, sua missão é cruzar os fatos,
@@ -39,8 +56,9 @@ function Home({}) {
                 verdadeiro assassino escape.
               </p>
             </p>
-
-            <button className={styles.button}>Iniciar investigação</button>
+            <div className={styles.buttonPos}>
+              <button className={styles.button}>Iniciar investigação</button>
+            </div>
           </div>
         </div>
       </div>
