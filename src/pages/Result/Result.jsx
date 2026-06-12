2f8211ef71aea9c688eee5de0c7bebabb26ec46d
marceloprmayrinck <marceloprmayrinck@gmail.com>
2026-06-12 14:13:33 -0300

    feat: implementação do card de crime

--- src/pages/Result/Result.jsx
+++ src/pages/Result/Result.jsx
@@ -1,4 +1,15 @@
-function Result () {
-    return <p>Result</p>
-};
+import { useContext } from "react";
+
+function Result() {
+  function ResultPage() {
+    const { investigation, reset } = useContext(InvestigationContext);
+    const { accusation } = investigation;
+    const correct = accusation?.correct;
+  }
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
 export default Result;
