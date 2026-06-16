ecba9cc744fe58f4b7edf2866297096a017d81e2
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-06-16 19:07:40 -0300

    ajustando borda do card, e espaçamento botao

--- src/components/WitnessCard/WhitnessCard.jsx
+++ src/components/WitnessCard/WhitnessCard.jsx
@@ -7,7 +7,7 @@ function WitnessCard({ witness, isRead, onMarkRead, isRevealed, onReveal, curren
     const isUnlocked = currentProgress >= witness.requiredProgress;
     
     return (
-        <div className={`${styles.witnessContainer} ${isRead ? styles.read : ''}`} onClick={isRevealed ? () => onMarkRead(witness.id) : undefined}>
+        <div className={`${styles.witnessContainer} ${isRead ? styles.read : ''}`} >
             <div className={styles.cardHeader}>
                 <div className={styles.headerName}>
                     <img className={styles.icon} src={iconHead} alt=""/>
@@ -19,14 +19,16 @@ function WitnessCard({ witness, isRead, onMarkRead, isRevealed, onReveal, curren
                 ) : !isRevealed ? (
                     <span 
                         className={styles.badgeReveal} 
-                        onClick={onReveal}
+                        onClick={() => {
+                            onReveal();
+                            onMarkRead(witness.id);
+                        }}
                     >
                         🕮 Revelar Depoimento
                     </span>
                 ) : (
                     <span 
                         className={styles.badgeRead}
-                        onClick={() => onMarkRead(witness.id)}
                     >
                         ✓ Analisado
                     </span>
@@ -36,21 +38,15 @@ function WitnessCard({ witness, isRead, onMarkRead, isRevealed, onReveal, curren
             {isRevealed && ( // exibe apenas se foi revelado
 
             <div className={`${styles.cardBody} ${styles.fadeIn}`}>
-
                 <p className={styles.witnessTestimony}>
                     "{witness.testimony}"
                 </p>
-
                 {witness.isContradictory && (
                     <span className={styles.alertText}>⚠️ Este depoimento contradiz um suspeito!</span>
                 )}
-
             </div>
-
             )}
-
         </div>
-
     );
 }
 
