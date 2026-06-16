fb39c9e1f6517fd85634dce81793da28e74a9588
Caio Vinicius Dias <dev.caiodias19@gmail.com>
2026-06-15 23:47:39 -0300

    Fix: corrigindo bug do bloqueado selecionável

--- src/components/WitnessCard/WhitnessCard.jsx
+++ src/components/WitnessCard/WhitnessCard.jsx
@@ -7,7 +7,7 @@ function WitnessCard({ witness, isRead, onMarkRead, isRevealed, onReveal, curren
     const isUnlocked = currentProgress >= witness.requiredProgress;
     
     return (
-        <div className={`${styles.witnessContainer} ${isRead ? styles.read : ''}`} onClick={() => onMarkRead(witness.id)} >
+        <div className={`${styles.witnessContainer} ${isRead ? styles.read : ''}`} onClick={isRevealed ? () => onMarkRead(witness.id) : undefined}>
             <div className={styles.cardHeader}>
                 <div className={styles.headerName}>
                     <img className={styles.icon} src={iconHead} alt=""/>
@@ -34,16 +34,23 @@ function WitnessCard({ witness, isRead, onMarkRead, isRevealed, onReveal, curren
             </div>
 
             {isRevealed && ( // exibe apenas se foi revelado
-            <div className={styles.cardBody}>
+
+            <div className={`${styles.cardBody} ${styles.fadeIn}`}>
+
                 <p className={styles.witnessTestimony}>
                     "{witness.testimony}"
                 </p>
+
                 {witness.isContradictory && (
                     <span className={styles.alertText}>⚠️ Este depoimento contradiz um suspeito!</span>
                 )}
+
             </div>
+
             )}
+
         </div>
+
     );
 }
 
